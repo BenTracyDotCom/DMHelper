9b85be38019d1d426819730a5d4c03c34db8324c
Ben Tracy <b.rob.tracy@gmail.com>
2023-06-02 14:50:03 -0400

    restructure for redux

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import Home from './src/screens/Home';
+import { store } from './src/redux/store';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-red-500">It's working! It's workiiiing!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Home />
+    </Provider>
   );
 }
 
---
+++ src/screens/Home.jsx
@@ -0,0 +1,66 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../redux/actions/countAction'
+
+export default function Home() {
+  const dispatch = useDispatch();
+
+  const count = useSelector((store) => store.count.count);
+
+  const handleIncreament = () => {
+    dispatch(increment());
+  };
+
+  const handleDecreament = () => {
+    dispatch(decrement());
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title_text}>Counter App</Text>
+      <Text style={styles.counter_text}>{count}</Text>
+
+      <TouchableOpacity onPress={handleIncreament} style={styles.btn}>
+        <Text style={styles.btn_text}> Increment </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={handleDecreament}
+        style={{ ...styles.btn, backgroundColor: '#6e3b3b' }}
+        // className="p-10 m-10"
+        >
+        <Text className="text-white"> Decrement </Text>
+      </TouchableOpacity>
+   </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    flexDirection: 'column',
+    padding: 50,
+  },
+  title_text: {
+    fontSize: 40,
+    fontWeight: '900',
+    marginBottom: 55,
+  },
+  counter_text: {
+    fontSize: 35,
+    fontWeight: '900',
+    margin: 15,
+  },
+  btn: {
+    backgroundColor: '#086972',
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  btn_text: {
+    fontSize: 23,
+    color: '#fff',
+  },
+});
