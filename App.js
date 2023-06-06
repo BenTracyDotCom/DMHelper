9cc78e2aff6d211e5d9c5744509c88061a71687d
Ben Tracy <b.rob.tracy@gmail.com>
2023-06-06 14:53:29 -0400

    womm

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import Home from './src/screens/Home';
 import { store } from './src/redux/store';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Home from './src/screens/Home';
+import Spells from './src/screens/Spells'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <Stack.Navigator>
+          <Stack.Screen
+          name="Home"
+          component={Home}
+          />
+          <Stack.Screen
+          name="Spells"
+          component={Spells}
+          />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
   );
 }
 
