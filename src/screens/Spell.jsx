afab967cc03ef25e38d15f09b437f3f937241c10
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-06-12 13:11:19 -0400

    Added some Spells functionality

--- src/screens/Spell.jsx
+++ src/screens/Spell.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { View, Text } from 'react-native';
 
-export default function Spell() {
+export default function Spell({data}) {
 
     return (
         <View>
--- src/screens/Spells.jsx
+++ src/screens/Spells.jsx
@@ -1,10 +1,43 @@
-import { Text, View } from 'react-native';
+import React, {useState, useEffect, useCallback} from 'react'
+import { Text, View, FlatList, ActivityIndicator, TouchableOpacity } from 'react-native';
 import Spell from '../components/Spell'
 
 export default function Spells() {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchSpells = async() => {
+      setLoading(true);
+      const response = await fetch('https://www.dnd5eapi.co/api/spells')
+      const json = await response.json()
+      let promises = json.results.map(spell => fetch(`https://www.dnd5eapi.co${spell.url}`));
+      let spellDetails = await Promise.all(promises.map(p => p.then(resp => resp.json())))
+      setData(spellDetails)
+      setLoading(false)
+    }
+
+    fetchSpells()
+  }, [])
+
+  if (loading) {
+  return <ActivityIndicator/>
+ }
   return (
     <View>
       <Text className="m-auto text-blue-700">Spells</Text>
+      <FlatList
+      data={data}
+      keyExtractor={(item) => item.index}
+      renderItem={({item}) => (
+        <View style={{margin: 10}}>
+          <Text style={{fontSize: 20}}>{item.name}</Text>
+          <Text style={{fontSize:16, color: 'grey'}}>{item.level}</Text>
+          <Text style={{fontSize: 16, color: 'grey'}}>{item.components.join(', ')}</Text>
+        </View>
+      )}
+      ></FlatList>
     </View>
   )
 }
