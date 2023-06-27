09677620733dd8f7ac0b19dcfba8d0c3d39c9da7
Ben Tracy <b.rob.tracy@gmail.com>
2023-06-27 09:56:13 -0400

    create CharacterList and pipe it to campaign page

--- src/features/campaigns/campaignSlice.js
+++ src/features/campaigns/campaignSlice.js
@@ -8,7 +8,41 @@ const campaignSlice = createSlice({
     quests: {
       "escort the cart": []
     },
-    currentQuest: "Escort the Cart"
+    currentQuest: "Escort the Cart",
+    characters: [
+      {
+        name: 'Snervelin',
+        type: 'pc',
+        race: 'Gnome',
+        class: 'Wizard',
+        level: '4'
+      },
+      {
+        name: 'Jealdor',
+        type: 'pc',
+        race: 'Dragonborn',
+        class: 'Barbarian',
+        level: '4'
+      },
+      {
+        name: "Boxbix",
+        type: 'npc',
+        race: 'Dwarf',
+        class: 'Cleric',
+        level: '4'
+      }
+    ],
+    npcs: [
+      {
+        name: 'Boxbix',
+        race: 'Dwarf',
+        class: 'Cleric',
+        stats: {
+          
+        }
+      }
+    ]
+    
   },
   reducers: {
     currentQuestUpdated: (state, action) => {
--- src/screens/Campaign.jsx
+++ src/screens/Campaign.jsx
@@ -1,11 +1,81 @@
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useSelector } from 'react-redux';
+import CharacterList from '../features/campaigns/CharacterList';
 
 export default function Campaign() {
-  return (
-    <View>
-      <Text>
-        Campaign screen
-      </Text>
-    </View>
-  )
-}
+
+  const campaign = useSelector(state => (state.campaign))
+  const [loading, setLoading] = useState(false)
+
+  if(loading){
+    return <StatusBar />
+  }
+
+  else {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text>Main Quest:</Text>
+          <Text>{Object.keys(campaign.quests)[0]}</Text>
+          <Text>Current Objective:</Text>
+          <Text>{campaign.currentQuest}</Text>
+        </View>
+        <View style={styles.contentContainer}>
+          <ScrollView style={styles.leftColumn}>
+            <View style={styles.scrollableContent}>
+              <Text>
+                <CharacterList />
+              </Text>
+            </View>
+          </ScrollView>
+          <View style={styles.rightColumn}>
+            <Text>
+              // Fixed height content goes here...
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    width: '100%',
+    height: 200,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    width: '100%',
+  },
+  leftColumn: {
+    flex: 1,
+    backgroundColor: 'white',
+    height: '100%',
+  },
+  scrollableContent: {
+    flexGrow: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  rightColumn: {
+    flex: 1,
+    backgroundColor: 'lightgray',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
