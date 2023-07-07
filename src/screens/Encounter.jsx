807469459dbfe7bb1e4994a4c941a14fc908b299
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-07-07 16:20:00 -0400

    Trying to get expo and Realm to get along

--- src/screens/Encounter.jsx
+++ src/screens/Encounter.jsx
@@ -1,8 +1,29 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Button } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import CharList from '../features/encounter/CharList';
 
+
 export default function Encounter(props) {
+
+    const useDispatch = useDispatch()
+
+
+    const [ttile, setTitle] = useState('')
+    const [target, setTarget] = useState('')
+    const [xpEarned, setXpEarned] = useState
+
+    const currentQuest = useSelector ((state) => state.campaign.currentQuest)
+
+    const submitEncounter = () => {
+        const newCounter = {
+            title,
+            active: 1,
+            target: Number(target),
+            xpEarned: Number(xpEarned),
+            loot: [],
+            chars: []
+        }
+    }
 
     return (
         <View>
