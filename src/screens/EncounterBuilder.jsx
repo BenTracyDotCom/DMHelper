d8aee38e2626708ba2bbca84f3973c866a533328
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-07-07 16:20:00 -0400

    Trying to get expo and Realm to get along

--- src/screens/EncounterBuilder.jsx
+++ src/screens/EncounterBuilder.jsx
@@ -1,10 +1,64 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react';
+import { View, Text, TextInput, TouchableOpacity, Button } from 'react-native'
+import {useSelector, useDispatch} from 'react-redux'
+import {currentQuestUpdated} from '../features/campaigns/campaignSlice.js'
+import Realm from 'realm';
+import {Encounter, Character} from '../data/encountersDB.js'
 
 export default function EncounterBuilder() {
+  const dispatch = useDispatch()
+
+  const [title, setTitle] = useState('')
+  const [target, setTarget] = useState('')
+  const [xpEarned, setXpEarned] = useState('')
+
+  const currentQuest = useSelector((state) => state.campaign.currentQuest)
+
+  const submitEncounter = () => {
+    const newEncounter = {
+      title,
+      active: 1,
+      target: Number(target),
+      xpEarned: Number(xpEarned),
+      loot: [],
+      chars: [],
+    }
+
+    let realm = new Realm({schema: [Character, Encounter]})
+
+    realm.write(() => {
+      realm.create('Encounter', newEncounter)
+    })
+
+    dispatch(currentQuestUpdated({...currentQuest, encounter: newEncounter}))
+
+    setTitle('')
+    setTarget('')
+    setXpEarned('')
+  }
 
   return (
     <View>
       <Text>Build yer Encounter Here</Text>
+      <TextInput
+      placeHolder="Title"
+      value={title}
+      onChangeText={(text) => setTitle(text)}
+      ></TextInput>
+      <TextInput
+        placeholder='Target'
+        value={target}
+        onChangeText={(text) => setTarget(text)}
+      />
+      <TextInput
+        placeholder='XP Earned'
+        value={xpEarned}
+        onChangeText={(text) => setXpEarned(text)}
+      />
+      <Button
+      title='Submit Encounter'
+      onPress={submitEncounter}
+      ></Button>
     </View>
   )
 }
