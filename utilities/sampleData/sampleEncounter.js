a4700866c982242b4dc48bf8a8c2fba40ba52e0b
Ben Tracy <b.rob.tracy@gmail.com>
2023-08-29 11:33:16 -0400

    fix merge conflicts

--- utilities/sampleData/sampleEncounter.js
+++ utilities/sampleData/sampleEncounter.js
@@ -2,7 +2,6 @@ import goblin from './goblin'
 import quasit from './sampleMonster'
 
 export default {
-<<<<<<< HEAD
     title: "Goblin Ambush",
     active: 0,
     target: 0,
@@ -71,55 +70,3 @@ export default {
       },
     ]
 }
-=======
-  title: "Goblin Ambush",
-  active: 0,
-  target: 0,
-  xpEarned: 0,
-  loot: [],
-  chars: [
-    {
-      name: "Snervelin",
-      ac: 14,
-      hp: 123,
-      status: [],
-      type: "pc",
-      initiative: 22,
-    },
-    {
-      name: "Goblin Shmn",
-      ac: 12,
-      hp: 30,
-      status: [],
-      type: "enemy",
-      initiative: 21,
-      cr: 3,
-    },
-    {
-      name: "Jealdor",
-      ac: 13,
-      hp: 30,
-      status: [],
-      type: "pc",
-      initiative: 18,
-    },
-    {
-      name: "Goblin A",
-      ac: 12,
-      hp: 24,
-      status: [],
-      type: "enemy",
-      cr: 0.25,
-      initiative: 13,
-    },
-    {
-      name: "Boxbix",
-      ac: 16,
-      hp: 32,
-      status: [],
-      type: "npc",
-      initiative: 8,
-    },
-  ],
-};
->>>>>>> main
